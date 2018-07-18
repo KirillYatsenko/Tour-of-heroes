cd5574f9c361676bc991b70eabb16b39ce4e6966
Yatsenko Kirill <yatsenko.kirill@outlook.com>
2018-07-18 15:17:13 +0200

    heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[] = [
+    {id: 1, name: "name#1"},
+    {id: 2, name: "name#2"},
+    {id: 3, name: "name#3"},
+    {id: 4, name: "name#4"},
+    {id: 5, name: "name#5"}
+];
