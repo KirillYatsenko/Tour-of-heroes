229f5184b3a03a19877b95a06b1af0de45e9f3c9
Yatsenko Kirill <yatsenko.kirill@outlook.com>
2018-07-19 09:54:27 +0200

    fetch heroes list async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +9,28 @@ import {HEROES} from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id : 1,
-    name: "Kirill Yatsenko"
-  };
+
+  // hero: Hero = {
+  //   id : 1,
+  //   name: "Kirill Yatsenko"
+  // };
   
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroSevice : HeroService) { }
 
+  getHeros() : void{
+    this.heroSevice.getHeroes()
+      .subscribe(x=>this.heroes = x);
+  }
+  
   ngOnInit() {
+    this.getHeros();
   }
 
 }
